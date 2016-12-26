68579c82497c44894b03827c730c3afa04b08fcf
Tobias Merkle <tgamerkle@gmail.com>
2016-12-26 14:04:28 -0500

    adjust chatbox

--- js/boot.js
+++ js/boot.js
@@ -9,8 +9,8 @@ boot.prototype = {
   },
 
   create: function() {
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.pageAlignHorizontally = true;
+//     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+//     this.scale.pageAlignHorizontally = true;
 //     this.scale.setScreenSize();
     this.game.state.start("preload");
   }
